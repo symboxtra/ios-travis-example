9d116f17e0b0217604f4fba592dea1a5fcd79079
Ignacio Sanchez Gines <nacho@geardome.com>
2016-04-29 18:15:13 +0200

    Escribimos un test que funciona siempre

--- TravisExampleTests/TravisExampleTests.swift
+++ TravisExampleTests/TravisExampleTests.swift
@@ -24,6 +24,9 @@ class TravisExampleTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let success = (1+1) == 2
+        XCTAssertTrue(success, "1 + 1 = 2")
     }
     
     func testPerformanceExample() {

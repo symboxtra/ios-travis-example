fc27fb2e00fe4035ccf849136219e84d66c99e77
Ignacio Sanchez Gines <nacho@geardome.com>
2016-04-29 16:34:34 +0200

    Proyecto inicial generado con el wizard

---
+++ TravisExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravisExample
+//
+//  Created by Ignacio Sánchez Ginés on 29/4/16.
+//  Copyright © 2016 Ignacio Sánchez Ginés. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
